f33779768d694e2b7dff4758dd2ccb55334c547f
pavankumar0406 <96548252+pavankumar0406@users.noreply.github.com>
2022-11-02 14:21:20 +0530

    update secret.py
    new code

--- secret.py
+++ secret.py
@@ -1,8 +1,11 @@
 import boto3
 
-client = boto3.client('secretsmanager')
+client = boto3.client('secretmanager')
+
+region_name = "ap-south-1"
 
 response = client.create_secret(
+    region_name=region_name
     Name = 'MyDBsecrets',
     SecretString='{"username":"admin", "password":"password123"}'
 )
