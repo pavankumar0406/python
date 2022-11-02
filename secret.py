9a42db0d5f5fe2a554c5e21dd1f89b9af72bff12
pavankumar0406 <96548252+pavankumar0406@users.noreply.github.com>
2022-11-02 13:06:11 +0530

    secret.py
    new code

---
+++ secret.py
@@ -0,0 +1,8 @@
+import boto3
+
+client = boto3.client('secretmanager')
+
+response = client.create_secret(
+    Name = 'My-DB-secrets'
+    SecretString='{"username":"admin", "password":"password123"}'
+)
