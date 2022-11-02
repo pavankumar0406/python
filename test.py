659843ba8b589c60d6355f46778c4b435cdcae30
pavankumar0406 <96548252+pavankumar0406@users.noreply.github.com>
2022-11-02 14:34:34 +0530

    create test.py
    new code

---
+++ test.py
@@ -0,0 +1,20 @@
+import boto3
+from botocore.exceptions import ClientError
+
+
+def get_secret():
+
+    region_name = "ap-south-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+        )
+    try: 
+      response = client.create_secret(
+    Name = 'MyDBsecrets',
+    SecretString='{"username":"admin", "password":"password123"}'
+    )
+get_secret()
